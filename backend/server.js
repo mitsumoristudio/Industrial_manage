b77a4b3bee3bbc3f6c45542d577c82a8de94b0b8
satoshimitsumori <smitsumori@hotmail.com>
2025-06-17 17:16:29 -0500

    cookie parser

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,13 @@ import projectRoute from "./routes/projectRoute.js";
 import contactRoute from "./routes/contactRoute.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import contactUsRoute from "./routes/contactUsRoute.js";
-import cookieParser from "cookie-parser";
+
 import express from "express";
 import dotenv from "dotenv";
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
+import cookieParser from "cookie-parser";
 // import sgMail from "@sendgrid/mail";
 
 dotenv.config();
