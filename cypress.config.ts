b716a1ff86ce1d83dc47b5bf53a74734598f101a
satoshimitsumori <smitsumori@hotmail.com>
2025-06-20 19:11:06 -0500

    getting ready for deployment

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,10 @@
-export default {
+//@ts-ignore
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000',
+    supportFile: false
   },
-};
+})
+
